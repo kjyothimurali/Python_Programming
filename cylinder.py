1fa86c2418227354cfda40c4bdfdb06ced4121e6
kjyothimurali <kjyothimurali1105@gmail.com>
2025-09-04 15:15:54 +0530

    Delete cylinder.py

--- cylinder.py
+++
@@ -1,5 +0,0 @@
-#finding volume of cylinder
-height=int(input("enter height of cylinder"))
-radius=int(input("enter base radius"))
-vol=(3.14)*radius*radius*height    #volume of cylinder is pi*r*r*h
-print("volume of cylinder : ",vol)
